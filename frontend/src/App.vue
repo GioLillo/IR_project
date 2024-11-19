69092d0701816c97667ae57ac29682efe7b8c413
Giulia <ambivg@usi.ch>
2024-11-19 18:55:16 +0100

    Started creating the search engine

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="../src/assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-gray-100 min-h-screen p-8">
+    <header class="text-center mb-8">
+      <h1 class="text-4xl font-bold text-indigo-600">Nanny Finder</h1>
+      <SearchBar @search="performSearch" />
+    </header>
+    <main>
+<!--      <SearchResults :results="results" :query="query" />-->
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-center mb-6">
+    <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search for nannies or babysitters..."
+        class="w-72 p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+    />
+    <button
+        @click="onSearch"
+        class="ml-4 px-6 py-3 bg-indigo-600 text-white rounded-lg shadow-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+    >
+      Search
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    onSearch() {
+      this.$emit("search", this.searchQuery);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
