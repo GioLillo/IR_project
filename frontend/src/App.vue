389a025dcc7e7c0bfe1a35e755f0dcac532bb8ff
Giulia <ambivg@usi.ch>
2024-11-21 12:46:22 +0100

    Changed search bar position

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class=" min-h-screen p-8">
+  <div class=" min-h-screen p-8 flex flex-col justify-center -mt-28">
     <header class="text-center mb-8">
       <h1 class="text-6xl font-bold text-violet-icon">Nannie Search</h1>
     </header>
     <main>
       <SearchBar @search="performSearch" />
-      <SearchResults :query="searchQuery" />
+      <!--      <SearchResults :query="searchQuery" />-->
     </main>
   </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar.vue'
-import SearchResults from "@/components/SearchResults.vue";
+// import SearchResults from "@/components/SearchResults.vue";
 
 export default {
   name: 'App',
   components: {
     SearchBar,
-    SearchResults
+    // SearchResults
   }
 }
 </script>
